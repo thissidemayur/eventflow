47f11907aeb952f3b8a15c551d76b43cd3f834dd
thissidemayur <thissidemayur@gmail.com>
2026-04-11 23:03:24 +0530

    feat: add validateEvent middleware to check validate incoming request - add validatedEvent in Express

---
+++ app/api/src/middleware/validate.ts
@@ -0,0 +1,18 @@
+import { EventSchema } from "@eventflow/shared"
+import {Request,Response,NextFunction} from "express"
+
+export function validateEvent(req:Request,res:Response,next:NextFunction) {
+
+    const result = EventSchema.safeParse(req.body)
+
+    if(!result.success){
+        const { fieldErrors } = result.error.flatten()
+        return res.status(400).json({
+          error: "Validation failed",
+          details: fieldErrors,
+        });
+    }
+
+    req.validatedEvent = result.data
+    next()
+}
--- app/api/src/types/express.d.ts
+++ app/api/src/types/express.d.ts
@@ -1,7 +1,9 @@
+import { EventInput } from "@eventflow/shared";
 import "express"
 declare module "express-serve-static-core" {
   interface Request {
     apiKeyId?: string;
     tenantId?: string;
+    validatedEvent?:EventInput
   }
 }
