d51b8fc701e0bd3fcb4a84135f47facf7139546d
thissidemayur <thissidemayur@gmail.com>
2026-04-12 17:52:40 +0530

    feat: add job queue in business-logic

--- app/api/src/routes/events.route.ts
+++ app/api/src/routes/events.route.ts
@@ -1,13 +1,25 @@
-import { Router,Response,Request } from "express";
+import { Router, Response, Request } from "express";
 import { authMiddleware } from "../middleware/auth";
 import { validateEvent } from "../middleware/validate";
+import { eventQueue } from "../config/queue";
 
 
 
 const router = Router()
 
-router.post("/events",authMiddleware,validateEvent,async(req:Request,res:Response)=>{
-
+router.post("/events", authMiddleware, validateEvent, async (req: Request, res: Response) => {
     // add bussiness logic + enqeue logic
-    res.status(202).json({message:"Event accepted"})
+    const job = await eventQueue.add("process-event", {
+        eventType:req.validatedEvent?.type!,
+        payload:req.validatedEvent?.payload!,
+        tenantId:req.tenantId!,
+        idempotencyKey :req.validatedEvent?.idempotencyKey,
+        apikeyId:req.apiKeyId!,
+        receivedAt: new Date().toISOString()
+    })
+
+    res.status(202).json({ activated:true,jobId:job.id  })
+
 })
+
+export const eventRouter = router
