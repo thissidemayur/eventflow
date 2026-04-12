38721125e330575dc58acf0718159a247c0d1c51
thissidemayur <thissidemayur@gmail.com>
2026-04-12 17:54:48 +0530

    fix: add pg-adaptor to run prisma/client@7

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,13 +1,31 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../.env"),
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
 export const prisma: PrismaClient =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ["error", "warn"] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error", "warn"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
