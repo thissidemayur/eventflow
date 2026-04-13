f210e6d43aa0dd59052a955bbbf8b4dee82d757d
thissidemayur <thissidemayur@gmail.com>
2026-04-13 08:03:03 +0530

    feat: implement Worker for bullmq

--- app/worker/src/index.ts
+++ app/worker/src/index.ts
@@ -1,1 +1,51 @@
 import "dotenv/config";
+import Redis from "ioredis";
+import path from "node:path";
+import dotenv from "dotenv";
+import { Worker } from "bullmq";
+import { EventJob, QUEUE_NAME } from "@eventflow/shared";
+import { processEvent } from "./processor";
+import { prisma } from "@eventflow/db";
+
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../.env"),
+});
+
+
+const REDIS_URL = process.env.REDIS_URL;
+if (!REDIS_URL) {
+  throw new Error("undefined REDIS_URL in env");
+}
+
+const connection = new Redis(REDIS_URL, {
+  maxRetriesPerRequest: null,
+});
+
+
+const worker = new Worker<EventJob>(QUEUE_NAME, processEvent, {
+  connection,
+  concurrency: 5, //process 5 job simulatenouslu
+});
+
+
+worker.on("completed",(job)=>{
+    console.log(`Job: ${job.id} completed!`)
+})
+
+worker.on("failed",(job,err)=>{
+  console.error(
+    `[Job ${job?.id} failed after ${job?.attemptsMade} attempts]: `,
+    err.message,
+  );
+
+  if(job) {
+    prisma.event.update({
+      where:{jobId:job.id},
+      data:{
+        status:"failed",lastError:err.message
+      }
+    }).catch(console.error)
+  }
+})
+
