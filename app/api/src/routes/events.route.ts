b7d83c79d55c82a9385df2431c7c40fa9d1df6e6
thissidemayur <thissidemayur@gmail.com>
2026-04-14 23:57:26 +0530

    feat: add ipratelimiting to block IP flood before touching actual auth

--- app/api/src/routes/events.route.ts
+++ app/api/src/routes/events.route.ts
@@ -2,12 +2,13 @@ import { Router, Response, Request } from "express";
 import { authMiddleware } from "../middleware/auth";
 import { validateEvent } from "../middleware/validate";
 import { eventQueue } from "../config/queue";
+import { ipRateLimit } from "../middleware/ipRateLimit";
 
 
 
 const router = Router()
 
-router.post("/events", authMiddleware, validateEvent, async (req: Request, res: Response) => {
+router.post("/events",ipRateLimit, authMiddleware, validateEvent, async (req: Request, res: Response) => {
     // add bussiness logic + enqeue logic
     const job = await eventQueue.add("process-event", {
         eventType:req.validatedEvent?.type!,
@@ -17,6 +18,7 @@ router.post("/events", authMiddleware, validateEvent, async (req: Request, res: 
         apikeyId:req.apiKeyId!,
         receivedAt: new Date().toISOString()
     })
+    console.log("JOB: ", job);
 
     res.status(202).json({ activated:true,jobId:job.id  })
 
