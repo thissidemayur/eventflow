e4ff84242a1322700dc1b92174631a7371ef7159
thissidemayur <thissidemayur@gmail.com>
2026-04-16 14:26:04 +0530

    feat: add logger in root file(index.ts)

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,10 +1,32 @@
-import express,{Response} from "express"
+import express, { NextFunction, Response, Request } from "express";
 import "dotenv/config";
 import { eventRouter } from "./routes/events.route";
+import { createLogger } from "@eventflow/shared";
+
 
 const app = express()
-app.use(express.json())
 
+const logger = createLogger("api")
+
+app.use((req:Request,res:Response,next:NextFunction)=>{
+  const start = Date.now()
+  res.on("finish",()=>{
+    logger.info({
+      method: req.method,
+      path: req.path,
+      status: res.statusCode,
+      duration: Date.now() - start,
+      apiKeyId:req.apiKeyId ?? null,
+      tenantId:req.tenantId ?? null,
+    },"request completed");
+
+  })
+
+  next()
+})
+
+app.use(express.json())
+app.set("trust proxy",false) // no proxy use
 app.use("/api/v1",eventRouter)
 app.get("/health",(_,res:Response)=>{
    res.json({
