388d13a3c763594da935b8ea21b8ef4405fc689d
thissidemayur <thissidemayur@gmail.com>
2026-05-20 16:30:16 +0530

    feat: add logger

--- app/api/src/middleware/auth.ts
+++ app/api/src/middleware/auth.ts
@@ -1,26 +1,38 @@
 import { Request, Response, NextFunction } from "express";
-import { hashApiKey } from "@eventflow/shared";
+import { createLogger, hashApiKey, metrics } from "@eventflow/shared";
 import { prisma } from "@eventflow/db";
+
+const logger = createLogger("api:auth")
 
 export async function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
+
   const apiKey = req.headers["x-api-key"] as string | undefined;
-  if (!apiKey) return res.status(401).json({ error: "Missing API key" });
-
-  const hashed = hashApiKey(apiKey);
-
-  const keyRecord = await prisma.apiKey.findUnique({
-    where: { keyHash: hashed },
-  });
-
-  if (!keyRecord || !keyRecord.active) {
-    return res.status(401).json({ error: "Invalid API key" });
+  if (!apiKey) {
+    metrics.increment("auth.missing_key")
+    return res.status(401).json({ error: "Missing API key" });
   }
 
-  req.apiKeyId = keyRecord.id
-  req.tenantId = keyRecord.tenantId
-  return next();
+ try {
+   const hashed = hashApiKey(apiKey);
+ 
+   const keyRecord = await prisma.apiKey.findUnique({
+     where: { keyHash: hashed },
+   });
+ 
+   if (!keyRecord || !keyRecord.active) {
+     return res.status(401).json({ error: "Invalid API key" });
+   }
+ 
+   req.apiKeyId = keyRecord.id
+   req.tenantId = keyRecord.tenantId
+   return next();
+ } catch (error) {
+    logger.error({error:(error as Error).message},"auth error")
+    metrics.increment("auth.error")
+    return res.status(500).json({ error: "Internal server error" });
+ }
 }
