eecea210e31918085b9309c8a57a632f65811be9
thissidemayur <thissidemayur@gmail.com>
2026-05-20 16:25:22 +0530

    feat: add logger

--- app/api/src/config/redis.ts
+++ app/api/src/config/redis.ts
@@ -1,9 +1,30 @@
+import { createLogger } from "@eventflow/shared";
 import { Redis } from "ioredis";
-import dotenv from "dotenv"
-import path from "node:path";
 
-dotenv.config({
-  path: path.resolve(process.cwd(), "../../.env"),
+const logger = createLogger("api:redis")
+
+
+function createRedisConnection(name:string, options:object = {}) {
+  const client = new Redis(process.env.REDIS_URL!,{
+    lazyConnect:true,
+    ...options
+  })
+
+  client.on("connect",()=>logger.info({connecttion:name},"redis connected"))
+  client.on("error", (err) =>
+    logger.error({ connection: name, error: err.message }, "redis error"),
+  );
+  client.on("connecting", () =>
+    logger.warn({ connection: name }, "redis reconnecting"),
+  );
+
+  return client;
+}
+
+// regular commands (rate limiting, caching)
+export const redis = createRedisConnection("api:general")
+
+// bullmq queue producer
+export const queueConnection = createRedisConnection("api:queue", {
+  maxRetriesPerRequest: null,
 });
-
-export const redis = new Redis(process.env.REDIS_URL!,{})
