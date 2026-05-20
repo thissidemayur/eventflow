4829a2aa8ee2509e077650f5eacb69312227404d
thissidemayur <thissidemayur@gmail.com>
2026-05-20 16:41:58 +0530

    feat: define eventjob types

--- packages/shared/src/types/queue.ts
+++ packages/shared/src/types/queue.ts
@@ -1,11 +1,12 @@
+import { Prisma } from "@prisma/client";
 
-export const QUEUE_NAME = "events"
+export const QUEUE_NAME = "events";
 
 export interface EventJob {
-    eventType: string;
-    payload: Record<string,unknown>
-    tenantId:string
-    apikeyId: string
-    idempotencyKey?: string
-    receivedAt: string
+  eventType: string;
+  payload: Prisma.InputJsonValue;
+  tenantId: string;
+  apikeyId: string;
+  idempotencyKey?: string;
+  receivedAt: string;
 }
