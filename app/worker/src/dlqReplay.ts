481f0e8d60c36e426648106cfcdddae31032b67f
thissidemayur <thissidemayur@gmail.com>
2026-05-20 16:40:03 +0530

    feat: add Redisinstance

--- app/worker/src/dlqReplay.ts
+++ app/worker/src/dlqReplay.ts
@@ -1,7 +1,8 @@
 import { QUEUE_NAME } from "@eventflow/shared";
 import { Queue } from "bullmq";
-import Redis from "ioredis";
+import RedisImport from "ioredis";
 
+const Redis = RedisImport.default;
 const REDIS_URL = process.env.REDIS_URL!;
 const dlqQueueConnection = new Redis(REDIS_URL, { maxRetriesPerRequest: null });
 const eventQueueConnection = new Redis(REDIS_URL, {
